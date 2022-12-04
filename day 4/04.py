790f166ee50d9553e1f069620d6c5f1d528085f4
atetz <mail@adamtetz.nl>
2022-12-05 00:03:51 +0100

    finished part 2.

--- day 4/04.py
+++ day 4/04.py
@@ -1,5 +1,5 @@
 """
-    part one: find overlapping sections
+    Day 4: Camp Cleanup
 """
 
 
@@ -9,7 +9,8 @@ def get_section_range(section):
     return {start} if start == stop else set(range(start, stop + 1))
 
 
-pairs = 0
+overlapping_pairs = 0
+overlapping_assignments = 0
 with open("day 4/04-input.txt") as f:
     for line in f:
         line = line.replace("\n", "")
@@ -17,6 +18,11 @@ with open("day 4/04-input.txt") as f:
         section_1 = get_section_range(pair[0])
         section_2 = get_section_range(pair[1])
         if section_1.issubset(section_2) or section_2.issubset(section_1):
-            print(True)
-            pairs += 1
-print(pairs)
+            overlapping_pairs += 1
+        for assignment in section_1:
+            if assignment in section_2:
+                overlapping_assignments += 1
+                break
+
+print(f"{overlapping_pairs} assignments fully contain the other.")
+print(f"{overlapping_assignments} assignments overlap with the other.")
